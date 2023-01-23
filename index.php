12ef96ad15664bbbfa28786d70579b3882736d3f
D.J Amarasinghe <maxwon555@gmail.com>
2023-01-23 18:06:56 +0530

    add new section

--- index.php
+++ index.php
@@ -122,8 +122,7 @@ include('includes/navbar.php');
       </div>
     </div>
 
-    <!-- Earnings (Monthly) Card Example
-    <div class="col-xl-3 col-md-6 mb-4">
+    <!--<div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-info shadow h-100 py-2">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
@@ -147,9 +146,8 @@ include('includes/navbar.php');
           </div>
         </div>
       </div>
-    </div> -->
+    </div>-->
 
-    <!-- Pending Requests Card Example
     <div class="col-xl-3 col-md-6 mb-4">
       <div class="card border-left-warning shadow h-100 py-2">
         <div class="card-body">
@@ -164,7 +162,7 @@ include('includes/navbar.php');
           </div>
         </div>
       </div>
-    </div> -->
+    </div> 
   </div>
 
   <!-- Content Row -->
